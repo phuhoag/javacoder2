c1574ce4389ee0e1855b8b681d21c1ca2b614f0e
PC <PC@MAYTINH>
2024-05-30 20:56:26 +0700

    bai5 vidu2

---
+++ Javacoder/src/Bai5/vidu2/MathHelper.java
@@ -0,0 +1,14 @@
+package Bai5.vidu2;
+
+public class MathHelper {
+
+	
+	
+	public int sum(int a, int b) {
+		return (a + b);
+	}
+	
+	public double sum(double a, double b) {
+		return (a+b);
+	}
+}
