7d50927469e03b28e6530e0a6e08677c8720d1d9
PC <PC@MAYTINH>
2024-05-30 20:52:49 +0700

    Bai5 vidu1

---
+++ Javacoder/src/Bai5/Animal.java
@@ -0,0 +1,10 @@
+package Bai5;
+
+public class Animal {
+
+	
+	public void speak() {
+		System.out.println("Animal is speaking...");
+	}
+	
+}
---
+++ Javacoder/src/Bai5/QuanLyDongVt.java
@@ -0,0 +1,15 @@
+package Bai5;
+
+
+public class QuanLyDongVt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Animal obj1 = new Animal();
+		obj1.speak();
+		Dog obj2 = new Dog();
+		obj2.speak();
+	}
+
+}
